a318abeb46623d2471444d20e5ef77a99fc24115
Manal-Bukhari <manalbukhari2233@gmail.com>
2025-05-09 12:14:59 +0500

    1-commit

--- api/index.js
+++ api/index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-});
+// Export the Express app as the default Serverless Function handler
+module.exports = app;
